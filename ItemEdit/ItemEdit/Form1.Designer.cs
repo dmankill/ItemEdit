7b28d47063adf62b5af07cde0adb682c5c5cf14f
dmankill <dmankill@qq.com>
2020-03-27 09:05:39 +0800

    加入中文mod读取

--- ItemEdit/ItemEdit/Form1.Designer.cs
+++ ItemEdit/ItemEdit/Form1.Designer.cs
@@ -31,13 +31,14 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Location = new System.Drawing.Point(268, 13);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(432, 21);
+            this.textBox1.Size = new System.Drawing.Size(177, 21);
             this.textBox1.TabIndex = 0;
             // 
             // button1
@@ -54,17 +55,29 @@
             // 
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 12;
-            this.listBox1.Location = new System.Drawing.Point(13, 41);
+            this.listBox1.Location = new System.Drawing.Point(13, 53);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(565, 400);
+            this.listBox1.Size = new System.Drawing.Size(565, 388);
             this.listBox1.TabIndex = 2;
             this.listBox1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseDoubleClick);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 14);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(245, 20);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.comboBox1.SelectionChangeCommitted += new System.EventHandler(this.comboBox1_SelectionChangeCommitted);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(590, 450);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
@@ -83,6 +96,7 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
 
--- ItemEdit/ItemEdit/Form1.cs
+++ ItemEdit/ItemEdit/Form1.cs
@@ -25,18 +25,32 @@ namespace ItemEdit
         private string[] files;
         private SortedDictionary<string, Item> sortedItems = new SortedDictionary<string, Item>();
         private List<ListBoxItem> cacheItems = new List<ListBoxItem>();
+        private List<string> soundList = new List<string>();
+        private Dictionary<string, string> transCh = new Dictionary<string, string>();
+        private string transPath = "";
+
         private void Form1_Load(object sender, EventArgs e)
         {
+            var fbd = new FolderBrowserDialog();
+
+            redo:
             path = System.Environment.CurrentDirectory + @"\user\mods\EmuTarkov-AllItemsExamined-1.0.0\db\items";
+            transPath = System.Environment.CurrentDirectory + @"\user\mods\EmuTarkov-LocaleCh-1.0.0\db\locales\ch\templates";
             if (!Directory.Exists(path))
             {
-                var fbd = new FolderBrowserDialog();
                 if (DialogResult.OK == fbd.ShowDialog())
                 {
                     string basepath = fbd.SelectedPath;
                     path = Path.Combine(basepath, @"user\mods\EmuTarkov-AllItemsExamined-1.0.0\db\items");
+                    transPath = Path.Combine(basepath, @"user\mods\EmuTarkov-LocaleCh-1.0.0\db\locales\ch\templates");
                 }
             }
+
+            if (!Directory.Exists(path))
+            {
+                goto redo;
+            }
+
             files = Directory.GetFiles(path);
             foreach (var file in files)
             {
@@ -47,8 +61,18 @@ namespace ItemEdit
                 {
                     continue;
                 }
+
+                if (item._props.ItemSound != null && !soundList.Contains(item._props.ItemSound))
+                {
+                    soundList.Add(item._props.ItemSound);
+                }
+
                 sortedItems.Add(file, item);
-            } 
+            }
+
+            comboBox1.BeginUpdate();
+            comboBox1.Items.AddRange(soundList.ToArray());
+            comboBox1.EndUpdate();
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
@@ -67,34 +91,32 @@ namespace ItemEdit
             listBox1.BeginUpdate();
             listBox1.Items.Clear();
             cacheItems.Clear();
-            
+
             foreach (var sortedItemsKey in sortedItems.Keys)
             {
-                if (sortedItems[sortedItemsKey]._name.Contains(text))
+                if (sortedItems[sortedItemsKey]._name.ToLower().Contains(text.ToLower()))
                 {
                     ListBoxItem listBoxItem = new ListBoxItem();
                     listBoxItem.FileName = sortedItemsKey;
-                    listBoxItem.Text = sortedItems[sortedItemsKey]._name;
+                    string name = GetTransName(sortedItemsKey);
+                    listBoxItem.Text = name;
                     cacheItems.Add(listBoxItem);
                     listBox1.Items.Add(listBoxItem);
                 }
-               
-            } 
+            }
+
             listBox1.DisplayMember = "Text";
 
             listBox1.EndUpdate();
-
-            
         }
 
         private void textBox1_KeyUp(object sender, KeyEventArgs e)
         {
-            
         }
-         
+
 
         private void button1_Click(object sender, EventArgs e)
-        { 
+        {
             var text = textBox1.Text;
             if (text.Length > 1)
             {
@@ -107,13 +129,64 @@ namespace ItemEdit
             int index = this.listBox1.IndexFromPoint(e.Location);
             if (index != System.Windows.Forms.ListBox.NoMatches)
             {
-                 
-                System.Diagnostics.Process.Start(((ListBoxItem)listBox1.SelectedItem).FileName);//双引号内填你想打开的文件路径
-
+                System.Diagnostics.Process.Start(((ListBoxItem) listBox1.SelectedItem).FileName); //双引号内填你想打开的文件路径
             }
             else
             {
-                listBox1.SelectedIndex = -1;//不做任何操作，将ListBox的选中项取消
+                listBox1.SelectedIndex = -1; //不做任何操作，将ListBox的选中项取消
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private string GetTransName(string key)
+        {
+            string name;
+            string fileName = Path.GetFileName(key);
+            string transFilePath = Path.Combine(transPath, fileName);
+            if (File.Exists(transFilePath))
+            {
+                string transString = File.OpenText(transFilePath).ReadToEnd();
+                var transItem = JsonConvert.DeserializeObject<TransItem>(transString);
+                name = transItem.Name;
+            }
+            else
+            {
+                name = sortedItems[key]._name;
+            }
+
+            return name;
+        }
+
+        private void comboBox1_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            var val = comboBox1.SelectedItem?.ToString();
+
+            if (val != null)
+            {
+                listBox1.BeginUpdate();
+                listBox1.Items.Clear();
+                foreach (var sortedItem in sortedItems)
+                {
+                    if (sortedItem.Value._props.ItemSound == val)
+                    {
+                        string name = GetTransName(sortedItem.Key);
+
+                        var lb = new ListBoxItem()
+                        {
+                            FileName = sortedItem.Key,
+                            Text = name
+                        };
+                        cacheItems.Add(lb);
+                        listBox1.Items.Add(lb);
+                    }
+                }
+
+                listBox1.DisplayMember = "Text";
+                listBox1.EndUpdate();
             }
         }
     }
